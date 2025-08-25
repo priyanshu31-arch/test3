105da36c228bf4fc806204d6dab2c4e537fbbce7
priyanshu31-arch <priyanshu310102@gmail.com>
2025-08-25 15:02:34 +0530

    update

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ import 'dotenv/config';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected... 🔌');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    console.log('MongoDB Connected Successfully...');
+  } catch(err){
+        console.log("Error connecting to database:", err);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
